edad6883348f6c761d561bf9d4ad555a8affdf79
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-11-04 10:33:30 +0000

    Add project files.

---
+++ SimpleCalculator/SimpleCalculator/Calculate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    //Need to implement the INotifyPropertyChanged Interface
+    //Allows me to register interest in events that change
+    //Attribute/Properties of elements on the UI
+    class Calculate : INotifyPropertyChanged
+    {
+        //Create Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Variables
+        private int arg1;
+        private int arg2;
+
+        //Entry 1
+        public int Arg1
+        {
+            get { return arg1; }
+            set 
+            { 
+                arg1 = value;
+                if(PropertyChanged != null)
+                {
+                    //Generate a property changed event in the system
+                    PropertyChanged(this, new PropertyChangedEventArgs("Answer"));
+                }
+            }
+        }
+
+        //Entry 2
+        public int Arg2
+        {
+            get { return arg2; }
+            set 
+            { 
+                arg2 = value;
+                if (PropertyChanged != null)
+                {
+                    //Generate a property changed event in the system
+                    PropertyChanged(this, new PropertyChangedEventArgs("Answer"));
+                }
+            }
+        }
+
+        //Read Only
+        public int Answer
+        {
+            get { return arg1 + arg2; }
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        Calculate calc;
+
+        public MainPage()
+        {
+            //Initialize
+            InitializeComponent();
+
+            //If calc is nothing
+            if(calc == null)
+            {
+                calc = new Calculate();
+            }
+
+            //Binding
+            this.BindingContext = calc;
+        }
+
+        private void EntArg1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Setting property when value changes
+            calc.Arg1 = Convert.ToInt32(EntArg1.Text);
+        }
+
+        private void EntArg2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Setting property when value changes
+            calc.Arg2 = Convert.ToInt32(EntArg2.Text);
+        }
+
+        private void BtnDoMaths_Clicked(object sender, EventArgs e)
+        {
+            LblAnswer.Text = (calc.Answer).ToString();
+        }
+    }
+}
